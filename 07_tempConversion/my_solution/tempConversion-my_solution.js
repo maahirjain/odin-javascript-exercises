46644a08eef69209c87f0e3258817bce7d5a1ccf
Maahir Jain <maahirdjain@gmail.com>
2024-06-14 17:31:39 +0530

    Implement my_solution for 07_tempConversion

--- 07_tempConversion/my_solution/tempConversion-my_solution.js
+++ 07_tempConversion/my_solution/tempConversion-my_solution.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInFahrenheit) {
+  tempInCelsius = (tempInFahrenheit - 32) * (5/9);
+  return Math.round(tempInCelsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInCelsius) {
+  tempInFahrenheit = tempInCelsius * (9/5) + 32;
+  return Math.round(tempInFahrenheit * 10) / 10;
 };
 
 // Do not edit below this line
