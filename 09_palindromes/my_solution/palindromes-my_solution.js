d6cc06d9ee30fd98443638f104cfd2107b0c8802
Maahir Jain <maahirdjain@gmail.com>
2024-06-17 02:53:30 +0530

    Implement my_solution for 09_palindromes

--- 09_palindromes/my_solution/palindromes-my_solution.js
+++ 09_palindromes/my_solution/palindromes-my_solution.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let i = 0;
+    let j = str.length - 1;
 
+    while (i < j) {
+        while (" .,:!?".includes(str.at(i))) { i++; }
+        while (" .,:!?".includes(str.at(j))) { j--; }
+        if (str.at(i).toLowerCase() === str.at(j).toLowerCase()) { i++; j--; }
+        else { return false; }
+    }
+
+    return true;
 };
 
 // Do not edit below this line
