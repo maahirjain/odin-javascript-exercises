d81bd804ebf86a0365f32875cbdc8a088049bf8e
Maahir Jain <maahirdjain@gmail.com>
2024-06-17 02:32:39 +0530

    Implement my_solution for 08_calculator

--- 08_calculator/my_solution/calculator-my_solution.js
+++ 08_calculator/my_solution/calculator-my_solution.js
@@ -1,25 +1,26 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((sum, num) => sum + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((product, num) => product * num, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+  return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0) { return 1; }
+  return num * factorial(num - 1);
 };
 
 // Do not edit below this line
