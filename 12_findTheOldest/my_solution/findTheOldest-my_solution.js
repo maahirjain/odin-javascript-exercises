7f9dd17aed0c19d1fb104b871b39b45af0d5f743
Maahir Jain <maahirdjain@gmail.com>
2024-06-17 03:16:28 +0530

    Implement my_solution for 12_findTheOldest

--- 12_findTheOldest/my_solution/findTheOldest-my_solution.js
+++ 12_findTheOldest/my_solution/findTheOldest-my_solution.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    return arr.reduce((oldestPerson, currentPerson) => {
+        if (!("yearOfDeath" in oldestPerson)) { 
+            oldestPerson.yearOfDeath = new Date().getFullYear(); 
+        }
 
+        if (!("yearOfDeath" in currentPerson)) { 
+            currentPerson.yearOfDeath = new Date().getFullYear(); 
+        }
+
+        currentPersonAge = currentPerson.yearOfDeath - currentPerson.yearOfBirth;
+        oldestPersonAge = oldestPerson.yearOfDeath - oldestPerson.yearOfBirth;
+
+        if (currentPersonAge > oldestPersonAge) { return currentPerson; }
+        else { return oldestPerson; }
+    }, arr[0]);
 };
 
 // Do not edit below this line
